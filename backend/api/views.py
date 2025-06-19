3ee2a351565b900e9f9b4f14d07bd277f87f43bd
Aswin <pratapsinghaswin@gmail.com>
2025-06-19 21:21:19 +0530

    test

--- backend/api/views.py
+++ backend/api/views.py
@@ -106,3 +106,5 @@ class RegisterView(viewsets.ViewSet):
     def check_session(self,request):
         return Response({'message':'Valid token'},status = status.HTTP_200_OK)
     
+def test():
+    return "Everything OKiedokie"
